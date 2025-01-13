f0ef3009ff1ee07288241dc2293c0f45f0adf7a8
Bob Dong <bdong9@jh.edu>
2025-01-13 15:59:43 -0500

    commit

---
+++ Tech-Lab-On-Campus/Topic-Exchange/solution/producer_sol.py
@@ -0,0 +1,20 @@
+import os
+import pika
+from producer_interface import mqProducerInterface
+
+class mqProducer(mqProducerInterface):
+    def __init__(self, routing_key: str, exchange_name: str) -> None:
+        self.routing_key = routing_key
+        self.exchange_name = exchange_name
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self) -> None:
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=con_params)
+        self.channel = self.connection.channel()
+        self.exchange = self.channel.exchange_declare(exchange=self.exchange_name, exchange_type = "topic")
+
+    def publishOrder(self, message: str) -> None:
+        self.channel.basic_publish(self.exchange_name, self.routing_key, message)
+        self.channel.close()
+        self.connection.close()
